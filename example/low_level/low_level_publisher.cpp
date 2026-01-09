6e556b1993b122f4f4ebb257268d612774c92b9e
Grégoire Passault <g.passault@gmail.com>
2026-01-09 21:13:08 +0100

    Updating example to try on k1

--- example/low_level/low_level_publisher.cpp
+++ example/low_level/low_level_publisher.cpp
@@ -7,8 +7,7 @@
 #include <chrono>
 #include <iostream>
 
-#define TOPIC "rt/DemoMsgTopic"
-#define SLEEP_TIME 1
+#define SLEEP_TIME 2
 
 using namespace booster::robot;
 using namespace booster_interface::msg;
@@ -18,7 +17,7 @@ int main() {
     ChannelPublisher<LowCmd> channel_publisher(b1::kTopicJointCtrl);
     channel_publisher.InitChannel();
     std::vector<MotorCmd> motor_cmds;
-    for (size_t i = 0; i < b1::kJointCnt; i++) {
+    for (size_t i = 0; i < 22; i++) {
         MotorCmd motor_cmd;
         motor_cmds.push_back(motor_cmd);
     }
@@ -30,21 +29,13 @@ int main() {
             low_cmd.motor_cmd()[i].q(0.0);
             low_cmd.motor_cmd()[i].dq(0.0);
             low_cmd.motor_cmd()[i].tau(0.0);
-            low_cmd.motor_cmd()[i].kp(0.0);
-            low_cmd.motor_cmd()[i].kd(0.0);
+            low_cmd.motor_cmd()[i].kp(10.0);
+            low_cmd.motor_cmd()[i].kd(1.0);
             low_cmd.motor_cmd()[i].weight(0.0);
-            if (i == int(b1::JointIndex::kHeadPitch)) {
-                low_cmd.motor_cmd()[i].q(0.785);
-                low_cmd.motor_cmd()[i].dq(0.0);
-                low_cmd.motor_cmd()[i].tau(0.0);
-                low_cmd.motor_cmd()[i].kp(4.0);
-                low_cmd.motor_cmd()[i].kd(1.0);
-                low_cmd.motor_cmd()[i].weight(1.0);
-            }
         }
 
         channel_publisher.Write(&low_cmd);
         std::cout << "Publish LowCmd" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(SLEEP_TIME));
+        std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
     }
 }
